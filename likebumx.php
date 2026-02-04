46d135ea010fb38df8ebc936f2300301fc95ed00
giabao1989 <giabao09041989@gmail.com>
2026-02-04 21:56:44 +0700

    Add files via upload

---
+++ likebumx.php
@@ -0,0 +1,53 @@
+$a5 = "authorization: $authu";
+$tsm99 = [$a1, $a2, $a3, $a4, $a5, $a6];
+
+
+$coixu = getk();
+if ($coixu === "AUTHU SAI" || $coixu === "AUTHU DIE"){echo "$coixu\n";;exit;}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$khi = -1; while (1) {$khi++;
+$test = chaytool();
+    if ($test === "FILE TRONG") {echo "DA HET COOKIE\n";exit;}
+elseif ($test === "HET TAI KHOAN") {break;}
+$tenfb  = $test['tenfb'];
+$cookie = $test['cokie'];
+$uid    = $test['uid'];
+$uta    = user_gen();
+$c66    = "user-agent: $uta";
+$fb14   = "Cookie: $cookie";
+$tsm21  = [$fb1, $fb2, $fb3, $fb4, $fb14];
+$tsm22  =  [$fb1, $fb2, $fb4, $ffb2, $fb14];
+
+echo ">>$tenfb\n";
+echo ">>$uid\n";
+echo ">>$cookie\n";exit;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+} /////// khi
