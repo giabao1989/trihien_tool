992070346b99f11449d0d35ac1dc9dfd67f264d1
giabao1989 <giabao09041989@gmail.com>
2026-02-04 18:21:16 +0700

    Add files via upload

---
+++ likebumx.php
@@ -0,0 +1,13 @@
+
+
+//$mobumx3 = explode("\n", explode("".$tkchay1.".txt"));
+//$mobumx2 = $mobumx
+
+
+$a5 = "authorization: $authu";
+$tsm99 = [$a1, $a2, $a3, $a4, $a5, $a6];
+
+
+$coixu = getk();
+if ($coixu === "AUTHU SAI" || $coixu === "AUTHU DIE"){echo "$coixu\n";;exit;}
+echo ">>>>>>>$coixu\n";
