db3605b3aef69474967284017981658a74500a7f
giabao1989 <giabao09041989@gmail.com>
2026-02-05 16:21:58 +0700

    Add files via upload

---
+++ likebumx.php
@@ -0,0 +1,218 @@
+
+
+
+
+
+
+
+
+while (1) {
+$dabe = date("dmYH");
+
+$authu = tkvmk();
+if ($authu === "SAI TAI KHOAN HOAC MAT KHAU") {echo"$authu\n";exit;}
+
+
+$a5 = "authorization: $authu";
+$tsm99 = [$a1, $a2, $a3, $a4, $a5, $a6];
+
+
+$coixu = getk();
+if ($coixu === "AUTHU SAI" || $coixu === "AUTHU DIE"){continue;}
+
+
+
+$eko++;
+if ($eko == 1) {benne();kengang1();}
+
+
+
+while (1) {$xoatk = [];
+$dabu = date("dmYH");
+if ($dabu === $dabe) {} else {echo ">>>>>>>>\n";break;}
+
+
+$khi = -1; while (1) {$khi++;$baku=0;
+$test = chaytool();
+    if ($test === "FILE TRONG") {echo "DA HET COOKIE\n";exit;}
+elseif ($test === "HET TAI KHOAN") {xoafb();break;}
+$tenfb  = $test['tenfb'];
+$cookie = $test['cokie'];
+$uid    = $test['uid'];
+$uta    = user_gen();
+$fb2    = "user-agent: $uta";
+$fb14   = "Cookie: $cookie";
+$tsm21  = [$fb1, $fb2, $fb3, $fb4, $fb14];
+$tsm22  = [$fb1, $fb2, $fb4, $ffb2, $fb14];
+$tsm80  = [$fb2];
+
+
+
+
+
+
+
+while (true) {
+if ($chnv == 1) {$nv='like_facebook';}
+if ($chnv == 2) {$nv='like_poster';}
+if ($chnv == 3) {$nv='';}
+
+
+$loa = loa();
+
+
+    if ($loa === "HET NV")  {echo"[$khi] $tenfb HET NV\r";sleep(3);mothai();break;}
+elseif ($loa === "HET JOB") {echo"[$khi] $tenfb HET JOB\r";sleep(3);mothai();break;}
+
+
+
+
+
+
+
+elseif ($loa === "JOB TRUA LAM") {}
+elseif($loa === "LAM COM") {$nv='like_poster';$loaa = loa();}
+
+
+
+
+
+$loa1 = loa1();
+
+
+
+$mi="-1"; while (true) {$mi++;
+$slj = $loa1['count'];
+$buff_id = $loa1['data'][$mi]['buff_id'];
+if ($buff_id == false) {echo"ĐÃ HẾT JOB\r";sleep(2);mothai();break;}
+if ($mi === 0) {echo "   $tenfb TÌM THẤY $slj JOB\r";sleep(2);mothai();}
+
+$nv1 = $loa1['data'][$mi]['type'];
+if ($nv1 == "like_facebook" || $nv1 == "like_poster") {} else {
+baoloi();sleep(2);mothai();continue;}
+$nv = $nv1;
+
+if ($chnv == 2) {if ($nv == "like_facebook") {
+echo"$nv DA BO QUA\r";baoloi();continue;}}
+
+
+
+////))
+$loa2 = loa2 ();
+if ($loa2 === "NHIEM VU DA XONG") {echo"$loa2\r";sleep(2);baoloi();mothai();continue;}
+if ($loa2 === "NHIEM VU LOI") {echo"$loa2\r";sleep(2);baoloi();mothai();continue;}
+
+if ($nv1 == "like_facebook") {
+$cxx = $loa2['loai'];
+$cx =camxuc_id();}
+
+
+$web1 = $loa2['web'];
+
+
+
+if ($nv1 == "like_facebook") {
+echo "$tenfb ĐÃ LẤY ĐUOC BÀI VIẾT\r";sleep(2);mothai();}
+
+if ($nv1 == "like_poster") {
+echo "$tenfb ĐÃ LẤY ĐUOC LINK VÀ BINH LUAN\r";sleep(2);mothai();
+$binhluan = $loa2['bl'];
+$com_id   = $loa2['c_id'];}
+
+
+
+
+
+
+$loanao = fb ();
+if ($loanao === "DIE COOKIE") {$loigi="DIE COOKIE";echo"\n";kengang3();sleep(3);mothai();xoafb($khi);break;}
+if ($loanao === "CHECKPOI 282" || $loanao === "CHECKPOI 956" || $loanao === "CHECKPOI 049"){
+$loigi = "CHECK POI";echo"\n";kengang3();xoafb($khi);break;}
+
+if ($loanao === "LOI NHE") {
+$uta = user_gen();
+$fb22 = "user-agent: $uta";
+$fb144   = "Cookie: $cookie";
+$tsm21  = [$fb1, $fb22, $fb3, $fb4, $fb144];
+echo"BAI VIET LOI\r";sleep(2);mothai();continue;}
+
+
+
+
+
+$fb_dtsg=$loanao['fb_sg'];
+$id = doilink();
+$dem1 = strlen($id);
+if ($dem1 < 3 || $dem1 > 30) {
+echo"DANG TIM LAI ID\r";sleep(3);mothai();
+$id = fbm();
+if ($id === "DA LOI ROI") {baoloi();echo"LAY UID LOI\r";sleep(1);mothai();continue;}
+}
+
+$uuid = "$uid$id";
+
+$chanlike = chanlike();
+if ($chanlike === "JOB NAY DA LAM ROI") {baoloi();echo"$chanlike\r";sleep(2);mothai();continue;}
+luuID();
+
+
+
+
+
+
+
+
+
+
+
+
+$notong = tonghop();
+    if ($notong === "CHECK POI") {$loigi="CHECKPOI";echo"\n";kengang3();sleep(1);baoloi();xoafb($khi);break;;}
+elseif ($notong === "THA TIM LOI") {echo"THA TIM LOI NHE\r";sleep(2);baoloi();continue;}
+elseif ($notong === "CHAN TT") {$loigi="CHẶN TUONG TAC";echo"\n";kengang3();sleep(1);baoloi();xoafb($khi);break;}
+elseif ($notong === "TRUA CO") {echo"TRUA CO\r";baoloi();continue;}
+elseif ($notong === "COMMENT BI AN") {echo"$notong\n";baoloi();continue;}
+elseif ($notong === "COMMENT THAT BAI") {echo"$notong\r";baoloi();sleep(2);mothai();continue;}
+echo "KET QUA $notong\r";sleep(2);mothai();
+
+
+
+
+
+
+
+
+
+
+
+
+
+$nhanbux = nhanbux();
+if ($nhanbux === "LOI NHE") {baoloi();continue;}
+$baku++;if($baku == 1){echo"\n";kengang();}
+$mtien = $nhanbux['mass'];
+$mlam  = $nhanbux['mm'];
+$coixu += $mtien;
+nhantong();tineo();
+continue;} ///) mi
+
+
+break;usleep(300000);} /////while cho loa
+
+} /////// khi
+
+
+
+tikeo();
+continue;}
+
+
+
+
+
+
+
+
+
+} /// while cho tkvmk
+
