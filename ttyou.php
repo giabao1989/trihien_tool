7b93f7743333c0e47b4666b910f03de259f86343
giabao1989 <giabao09041989@gmail.com>
2026-03-22 00:12:24 +0700

    Add files via upload

---
+++ ttyou.php
@@ -0,0 +1,37 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function urltup() {global $tsm, $url;
+$url = "https://gateway.golike.net/api/youtube-account";
+$goi = get_url($url, $tsm)['data'];
+print_r($goi);}
+
+
+
+
+$i=0;
+
+
+$monao3 = explode("\n", @file_get_contents('tkgo.txt'));
+$monao2 = $monao3[$i];
+$token  = explode('|', $monao2)[0];
+if ($token == false) {exit;}
+$user   = explode('|', $monao2)[1];
+$tsm = tsm();
+
+
+
+
+urltup();
